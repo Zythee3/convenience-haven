918b8564763485c86e1c784a6b627a08978e62d4
Zythee3 <zmatheusguilherme2@gmail.com>
2023-11-12 18:01:36 -0300

    Update Modulo 01.py

--- Modulo 01.py
+++ Modulo 01.py
@@ -1,10 +1,15 @@
 import random
 import string
+
 class itens:
-    def __init__ (self, nome, descricao, condicao):
+    def __init__ (self, nome, descricao, condicao, id, doacao):
         self.nome = nome
         self.descricao = descricao
         self.condicao = condicao
+        self.id = id
+        self.doacao = doacao
+
+itens_registrados = []
         
 def cria_arquivo(nome_item):
     nome_item + ".txt"
@@ -17,14 +22,26 @@ def cria_item():
     descricao_item = input("Descrição do produto: ")
     condicao_item = input("Condição atual do produto: ")
     id_letra_produto = ""
+    while True:
+        try:
+            escolha_doacao = int(input("[0] doar\n[1] vender\n"))
+            if escolha_doacao >=0 and escolha_doacao <=1:
+                break
+        except ValueError:
+            print("Opção inválida, tente novamente")
+        
     for x  in range(4):
         id_letra_produto += random.choice(string.ascii_letters)
     
     id_numero_produto = random.randint(0, 999)
     id_produto =f"{id_letra_produto} - {id_numero_produto}"
-    dados_item = f"Nome: {nome_item}\nDescricao: {descricao_item}\nCondicao: {condicao_item}\nID: {id_produto}"
+
+    dados_item = f"Nome: {nome_item}\nDescricao: {descricao_item}\nCondicao: {condicao_item}\nID: {id_produto}\nOpcao doar(0)/vender(1): {escolha_doacao}"
     escreve_arquivo(nome_item, dados_item)
+    novo_item = itens(nome_item, descricao_item, condicao_item, id_produto, escolha_doacao)
     
+    itens_registrados.append(novo_item)
+
 
 def escreve_arquivo(nome_item, conteudo):
     nome_item + ".txt"
@@ -32,6 +49,7 @@ def escreve_arquivo(nome_item, conteudo):
         arquivo.write(conteudo)
 
 cria_item()
+
 
 
 
