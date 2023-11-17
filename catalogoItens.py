86b8f02c7358052a36f66776639a41ed1f277896
mfhhia <nandahiarita.15@gmail.com>
2023-11-17 20:26:34 -0300

    commit para atualizar o vscode

--- catalogoItens.py
+++ catalogoItens.py
@@ -11,19 +11,67 @@ class itens:
         
         
 
-def ler_arquivo():
-    try: 
+def ler_itens():
+    try:
         with open("itensregistrados.csv", 'r') as arquivo:
-            # leitor = csv.reader(arquivo)
-            leitor = csv.DictReader(arquivo)
-            for linha in leitor:
-                
+            ler = csv.DictReader(arquivo)
+            
+            print('Catálogo de itens:')
+            print('------------------------------')
+            for coisa in ler:
+                nomeItem = coisa['Nome do item']
+                idItem = coisa['id']
+                creditos = coisa['Creditos']
+                print(f'Item: {nomeItem}\nID do item: {idItem}\nCréditos retornados: {creditos}')
                 
     except FileNotFoundError:
-        print(f'O arquivo {caminho_do_arquivo} não foi encontrado.')
+        print(f'O arquivo itensregistrados não foi encontrado.')
     except Exception as e:
         print(f'Ocorreu um erro: {e}')
+  
+  
+def encontrarCliente(nome_cliente):
+    try:
+        with open('itenscadastrados', 'r') as arquivo:
+            ler = csv.DictReader(arquivo)
+            for coisa in ler:
+                if coisa['Nome do cliente'] == nome_cliente:
+                    return True
+        return False
+    except FileNotFoundError:
+        print(f'O arquivo itensregistrados não foi encontrado.')
+    except Exception as e:
+        print(f'Ocorreu um erro: {e}')
+  
+def trocarItens(id_item):
     
-
-ler_arquivo()
+    nome_cliente = input('Insira o seu nome:')
+    if encontrarCliente(nome_cliente):
+        try:
+            with open('itensregistrados.csv','r') as arquivo:
+                ler = csv.DictReader(arquivo)
+                lista = list(ler)
+                nome_item = None
+                creditos_necessarios = 0
+                for linha in lista:
+                    if linha['id do produto'] == id_item:
+                        nome_item = linha['Nome do item']
+                        
+                        creditos_necessarios = int(linha.get('Creditos',0))
+                        if creditos_necessarios <= 0:
+                            print(f' O item com o ID {nome_item} ainda não possui créditos adquiridos.')
+                            return 
+                        creditos_cliente = int(linha.get('Creditos',0))
+                        elif creditos_necessarios > creditos_cliente:
+                            print(f'Cliente {nome_cliente} não possui créditos sulficientes para trocar pelo item.')
+                            return
+                        else:
+                            linha['Creditos'] = creditos_necessarios
+                        
+def main():
+    ler_itens()
+    id_item = input('Insira o ID do item que deseja: ')
+    
+    
+            
 
--- gestaodecreditos.py
+++ gestaodecreditos.py
@@ -1,14 +1,55 @@
 import csv
-
-def adicionar_creditos(creditosporitem):
-    try: 
+# from itensregistrados import *
+def adicionar_creditos(credporitem):
+    try:
         with open("itensresgitrados", 'r') as arquivo:
-            leitor = vsc.reader(arquivo)
+            ler = csv.DictReader(arquivo)
+
+            item = list(ler)
+
+            item[0]['Creditos'] = 'Creditos'
+            # coisa é uma variavel para que eu possa interagir com o arquivo 
+            for coisa in item:
+                nomeItem = coisa['Nome do item']
+                
+                    # atribuir credito ao item
+                creditos = credporitem.get(nomeItem, 0)
+                coisa['Creditos'] = creditos
+        
+        with open('itensresgitrados','w',newline =' ') as arquivoModificado:
+            escrever = csv.DictReader(arquivoModificado, fieldnames = ler.fieldnames + ['Creditos'])
+            escrever.writeheader()
+            escrever.writerows(item) 
+    except FileNotFoundError:
+        print(f'O arquivo itensregistrados.csv não foi encontrado.')
+    except Exception as e:
+        print(f'Ocorreu um erro: {e}')   
+        
+def mostrarItens():
+    try:
+        with open('itensregistrados', 'r') as arquivo:
+            ler = csv.DictReader(arquivo)
+
+            print('itens e seus status de aprovação:')
+            print('-------------------------------------') 
+            for coisa in ler:
+                nomeItem = coisa['Nome do item']
+                aprovacao = coisa['aprovacao']
+                print(f'Item: {nomeItem}, Aprovação: {aprovacao}')
+    except FileNotFoundError:
+        print(f'O arquivo itensregistrados.csv não foi encontrado.')
+    except Exception as e:
+        print(f'Ocorreu um erro: {e}')  
+        
+def main(args=None):
+    mostrarItens()
+    
+    
+main()      
             
-            linhas= list(leitor)
-            
-        with open("itensregistrados", 'w', newline=' ') as arquivonovo:
-            escrito = csv.writer(arquivonovo)
-            
-            
+        
+        
+                
+
+
     
